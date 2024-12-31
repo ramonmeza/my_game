28c94cc577d168b19009c78559b0ac006c02a871
Ramon Meza <ramon_meza@live.com>
2024-12-31 00:02:59 -0500

    Documentation generation

---
+++ docs/conf.py
@@ -0,0 +1,37 @@
+"""
+The classic card game Blackjack, implemented in Python. 
+Copyright (C) 2024  Ramon Meza
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import sys
+
+sys.path.insert(0, "../src")
+
+project = "BlackjackPy"
+copyright = "2024, Ramon Meza"
+author = "Ramon Meza"
+
+extensions = [
+    "autoapi.extension",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
+
+autoapi_dirs = ["../src/blackjackpy"]
---
+++ docs/index.rst
@@ -0,0 +1,17 @@
+.. BlackjackPy documentation master file, created by
+   sphinx-quickstart on Mon Dec 30 23:43:33 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+BlackjackPy documentation
+=========================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
